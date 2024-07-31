d15d727ba15aaee3dabf5259350b1cb0a2ad9819
7erra <ichbinterra@gmail.com>
2024-07-31 14:45:35 +0200

    Migrate /web/templates

--- server/scenario/web_api/scenario.py
+++ server/scenario/web_api/scenario.py
@@ -1,22 +1,25 @@
-from apiflask import APIBlueprint
-
 import models
+from apiflask import APIBlueprint, Schema
+from apiflask.fields import Integer, Nested, String
 from utils.decorator import admin_only
 
 web_api = APIBlueprint("web_api-scenario", __name__)
 
 
+class ExecutionOut(Schema):
+    id = Integer()
+    name = String()
+
+
+class TemplateOut(Schema):
+    id = Integer()
+    name = String()
+    executions = Nested(ExecutionOut(many=True))
+
+
 @web_api.get("/templates")
 @admin_only
+@web_api.output(TemplateOut(many=True))
 def get_templates():
-    return [
-        {
-            "id": scenario.id,
-            "name": scenario.name,
-            "executions": [
-                {"id": execution.id, "name": execution.name}
-                for execution in scenario.executions
-            ],
-        }
-        for scenario in models.Scenario.query
-    ]
+    "Returns a list of available scenarios and their associated executions"
+    return models.Scenario.query
